cd990c2405808f47a99517469d3098904d8e6dbd
twosmalltrees <harrison@twosmalltrees.com>
2016-03-25 14:03:24 +1100

    Add conversations_users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325023224) do
+ActiveRecord::Schema.define(version: 20160325030222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20160325023224) do
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "conversations_users", force: :cascade do |t|
+    t.integer "conversation_id"
+    t.integer "user_id"
   end
 
   create_table "giphs", force: :cascade do |t|
