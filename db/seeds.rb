74cf88524ba06ce0db73a36dba2d28c6a6f69af5
twosmalltrees <harrison@twosmalltrees.com>
2016-03-26 09:16:28 +1100

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+u1 = User.create :email => 'harrison@example.com', :password => 'chicken', :password_confirmation => 'chicken'
+u2 = User.create :email => 'josh@example.com', :password => 'chicken', :password_confirmation => 'chicken'
+u3 = User.create :email => 'brynley@example.com', :password => 'chicken', :password_confirmation => 'chicken'
+#
+# Contact.destroy_all
+# u1.contacts << (Contact.create :contact_id => u2.id)
+# u1.contacts << (Contact.create :contact_id => u3.id)
+
+Conversation.destroy_all
+c1 = Conversation.create :name => 'South Street'
+c2 = Conversation.create
+c3 = Conversation.create
+
+c1.users << u1 << u2 << u3
+c2.users << u1 << u2
+c3.users << u1 << u3
