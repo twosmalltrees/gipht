171b763f962eb5a0a5b038c34df575588f68a630
twosmalltrees <harrison@twosmalltrees.com>
2016-03-25 17:43:43 +1100

    Add new user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = @current_user
+  end
+
+  def update
+    user = @current_user
+    user.update user_params
+    redirect_to root_path
+  end
+
+  # Index method? Not yet.
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation, :profile_pic)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  get '/sign_up' => 'users#new'
+
+  get '/sign_in' => 'sessions#new'
+  post '/sign_in' => 'sessions#create'
+  delete '/sign_in' => 'sessions#destroy'
 end
